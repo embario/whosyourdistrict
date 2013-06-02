a12760223d1a7fd90a745a405a75ece4f5313de9
Andy Sayler <andy.sayler@gmail.com>
2013-06-02 01:56:36 +0000

    Removed debugging

--- application.py
+++ application.py
@@ -7,16 +7,12 @@ app.debug = True
 
 @app.route("/")
 def esri():
-    print("Called root endpoint")
     return render_template(HTML_HOME)
 
 @app.route("/data/pop/total", methods=['GET'])
 def data_pop_total():
-    print("Called /data/pop/total endpoint")
     state = request.args.get('state')
-    print("state = " + str(state))
     district = request.args.get('district')
-    print("district = " + str(district))
     usc = USCensus_API_CD()
     return jsonify(usc.getPopTotal(state, district))
 
